8c18d4356c64eb748f47284299ae8ce80311e4c2
koreaygj <ygj1828@gmail.com>
2023-11-16 15:41:46 +0900

    add: react-router-dom & home page, product-detail page

---
+++ src/routes/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <h1>
+      <span>HOme....</span>
+    </h1>
+  );
+}
+
+export default Home;
---
+++ src/routes/ProductDetail.js
@@ -0,0 +1,9 @@
+function ProductDetail() {
+  return (
+    <h1>
+      <span>ProductDetail</span>
+    </h1>
+  );
+}
+
+export default ProductDetail;
